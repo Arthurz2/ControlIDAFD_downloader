892472ae32482532aed329667b59981a434e1f09
Arthur Eduardo <126907230+Arthurz2@users.noreply.github.com>
2025-05-14 09:14:04 -0300

    Update userLojaCad.js

--- server/scripts/userCad/userLojaCad.js
+++ server/scripts/userCad/userLojaCad.js
@@ -1,36 +1,36 @@
 export const lojasDownload = {
     DownloadAfd01: {
-        ip: '192.168.25.90',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: '1234',
     },
     DownloadAfd02: {
-        ip: '192.168.22.153',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: '1234',
     },
     DownloadAfd03: {
-        ip: '192.168.23.178',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: '12345',
     },
     DownloadAfd04: {
-        ip: '192.168.15.183',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: 'admin',
     },
     DownloadAfd05: {
-        ip: '192.168.5.30',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: '1234',
     },
     DownloadAfd06: {
-        ip: '192.168.0.62',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: 'admin',
     },
     DownloadAfd07: {
-        ip: '192.168.27.152',
+        ip: '192.168.0.0',
         login: 'admin',
         senha: '1234',
     }
